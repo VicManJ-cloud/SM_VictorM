9a41b6b3bc84dca7df76aaa56458071864924475
VicManJ-cloud <victor.itam.mx@gmail.com>
2026-09-18 12:04:24 -0600

    Comprobacion del ejemplo del potenciometro

--- Robotics_ws/src/colmibot_firmware/esp32_basics/ADC_Pot/ADC_Pot.ino
+++ Robotics_ws/src/colmibot_firmware/esp32_basics/ADC_Pot/ADC_Pot.ino
@@ -1,11 +1,17 @@
+//se asocia el valor 15 a la palabra pot. en ese pin se conecta el pin de en medio
+//del potenciometro
 #define POT 15
 
 void setup() {
+	//se inicia el puerto a 115200 baudios
   Serial.begin(115200);
 }
-
+//se inicia un loop indefinido
 void loop() {
+	//analogRead convierte el voltaje del pin a un numero
   int valor = analogRead(POT);
+  //se manda el valor del puerto serial junto con salto de linea para separar un valor de otro
   Serial.println(valor);
+  //se esperan 100 ms
   delay(100);
 }
